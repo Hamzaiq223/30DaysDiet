120bf6f3b39a563e179012809abc1e77f3c60264
mac <mac@macs-mbp-2.home>
2023-06-15 21:24:40 +0500

    New Functionality Added

---
+++ app/src/main/java/com/example/a30daysdiet/Fragments/DietPlan.java
@@ -0,0 +1,30 @@
+package com.example.a30daysdiet.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a30daysdiet.R;
+
+
+public class DietPlan extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_diet_plan, container, false);
+    }
+}
--- app/src/main/java/com/example/a30daysdiet/MainActivity.java
+++ app/src/main/java/com/example/a30daysdiet/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.a30daysdiet;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.a30daysdiet.Fragments.Days;
+import com.example.a30daysdiet.Fragments.DietPlan;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    FrameLayout frameLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.my_drawer_layout);
+         navigationView = findViewById(R.id.navigation_view);
+         frameLayout = findViewById(R.id.fragment_container);
+
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            selectDrawerItem(menuItem);
+            return true;
+        });
+
+        Days days = new Days();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, days).commit();
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.nav_open,
+                R.string.nav_close
+        );
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    private void selectDrawerItem(MenuItem menuItem) {
+        // Load the appropriate fragment into the frame layout based on the menu item selected
+        switch (menuItem.getItemId()) {
+            case R.id.nav_diet_plan: {
+                DietPlan dietPlan = new DietPlan();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, dietPlan).commit();
+                getSupportActionBar().setTitle("Diet Plan");
+                break;
+            }
+
+        }
+
+        // Highlight the selected item in the navigation menu and close the drawer
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
